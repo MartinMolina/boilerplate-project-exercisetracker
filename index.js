698188d38953bd6e69b89cbdecc562cd6bb79e59
Martín Molina <martindmolina@live.com.ar>
2024-04-27 18:58:41 +0000

    database connection, schema and model creation

--- index.js
+++ index.js
@@ -10,9 +10,25 @@ app.get('/', (req, res) => {
 });
 
 
+// My code
+const mongoose = require('mongoose');
 
+mongoose.connect(process.env.MONGO_URI);
 
+const userSchema = new mongoose.Schema({
+  username: {type: String, required: true, unique: true}
+});
+const exerciseSchema = new mongoose.Schema({
+  username: String,
+  description: {type: String, required: true},
+  duration: {type: Number, required: true},
+  date: Date,
+  user_id: {type: String, required: true}
+});
+
+const User = mongoose.Model("User", userSchema);
+const Exercise = mongoose.Model("Exercise", exerciseSchema);
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
-})
+});
