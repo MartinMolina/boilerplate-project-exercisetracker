627e1271712bd363128269329a3e958709c51001
Martín Molina <martindmolina@live.com.ar>
2024-04-28 09:15:58 +0000

    GET to /api/users/:_id/logs

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.get('/', (req, res) => {
 
 // My code
 const mongoose = require('mongoose');
+const { use } = require('express/lib/application')
 
 mongoose.connect(process.env.MONGO_URI);
 
@@ -21,11 +22,10 @@ const userSchema = new mongoose.Schema({
   username: {type: String, required: true, unique: true}
 });
 const exerciseSchema = new mongoose.Schema({
-  username: String,
+  user_id: {type: String, required: true},
   description: {type: String, required: true},
   duration: {type: Number, required: true},
-  date: Date,
-  user_id: {type: String, required: true}
+  date: Date
 });
 
 const User = mongoose.model('User', userSchema);
@@ -34,12 +34,12 @@ const Exercise = mongoose.model('Exercise', exerciseSchema);
 app.post('/api/users', (req, res) => {
   const name = req.body.username;
   User.findOne({username: name})
-  .then((doc) => {
+  .then(doc => {
     if(doc)
       res.json({username: doc.username, _id: doc._id});
     else {
       new User({username: name}).save()
-      .then((nDoc) => {
+      .then(nDoc => {
         res.json({username: nDoc.username, _id: nDoc._id});
       }, (err) => {
         res.send(err);
@@ -49,8 +49,47 @@ app.post('/api/users', (req, res) => {
 });
 
 app.get('/api/users', (req, res) => {
-  User.find().then((users) => {
+  User.find().then(users => {
     res.send(users);
+  });
+});
+
+app.post('/api/users/:_id/exercises', (req, res) => {
+  User.findOne({_id: req.params._id}).then(doc => {
+    if(!doc)
+      res.json({error: "User does not exist"});
+    else {
+      var date = req.body.date.trim();
+      new Exercise({
+        user_id: req.params._id,
+        description: req.body.description,
+        duration: req.body.duration,
+        date: date == '' ? new Date() : new Date(date)
+      }).save()
+      .then(nDoc => {
+        res.json({_id: doc._id, username: doc.username, date: nDoc.date.toDateString(), duration: nDoc.duration, description: nDoc.description});
+      }, err => {
+        res.send(err);
+      })
+    }
+  })
+});
+
+app.get('/api/users/:_id/logs', (req, res) => {
+  User.findById(req.params._id)
+  .then(user => {
+    Exercise.where({user_id: user._id}).countDocuments()
+    .then(count => {
+      Exercise.find({user_id: user._id})
+      .then(docs => {
+        let mappedDocs = docs.map(e => ({
+          description: e.description,
+          duration: e.duration,
+          date: e.date.toDateString()
+        }));
+        res.json({_id: user._id, username: user.username, count: count, log: mappedDocs});
+      });
+    });
   });
 });
 
